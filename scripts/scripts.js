d419b4d92029dd6317b2fd7b88c7fce2760917c6
pbi <163220914+pbiggin@users.noreply.github.com>
2024-05-24 21:59:29 +1000

    added styling effects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("fadein").classList.add("active");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function(){ 
+    document.getElementById("lightApp").classList.add("active");
+  }, 1500);
+});
+
+
+
+function revealProjects() {
+  document.getElementById("projectHide").classList.toggle("on");
+  document.getElementById("projectBtn").classList.toggle("off");
+}
